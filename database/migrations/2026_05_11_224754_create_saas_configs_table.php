af27ee33bb633f3084aee124df620b2442abc0d1
mascahyo15 <mascahyo15@gmail.com>
2026-05-12 05:50:49 +0700

    migrasi improve

---
+++ database/migrations/2026_05_11_224754_create_saas_configs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('saas_configs', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('key');
+            $table->enum('type', ['text', 'description']);
+            $table->text('value');
+            $table->text('descripton')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('saas_configs');
+    }
+};
