0e165ee795336d1de647c85dd026ab2be71e23d4
mascahyo1 <mascahyo15@gmail.com>
2026-05-12 07:06:19 +0700

    admin saas

---
+++ database/migrations/2026_05_12_000505_create_admin_saas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admin_saas', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone_country_code');
+            $table->string('phone_number');
+            $table->boolean('is_active')->default(true);
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admin_saas');
+    }
+};
