b68fde026f40d75673a1049b550b2d9efd7ae954
mascahyo1 <mascahyo15@gmail.com>
2026-06-30 08:28:56 +0700

    bugdfix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+<meta http-equiv="Content-Security-Policy"
+  content="trusted-types vue gIqNx7 default default2 'allow-duplicates'">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
